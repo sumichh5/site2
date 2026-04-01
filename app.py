9d7245793b1c2be0118b9e0840670185c893ae1e
sumichh5 <sumichh5@gmail.com>
2026-04-01 18:57:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, redirect
-import subprocess
+import threading
 import os
+import bot  # импорт твоего файла
 
 app = Flask(__name__)
 
-# запуск бота
-subprocess.Popen(["python", "bot.py"])
+# 🔥 запускаем бот в отдельном потоке
+threading.Thread(target=bot.main).start()
 
 @app.route('/')
 def index():
     return redirect("https://discord.gg/2cU2wynKK")
 
-# 🔥 ВАЖНО
 port = int(os.environ.get("PORT", 8080))
-
 app.run(host="0.0.0.0", port=port)
